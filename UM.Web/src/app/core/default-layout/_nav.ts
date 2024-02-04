c6118c015f51227716f3cb39639a27b951382ffa
MD Saiful Islam <saifulprogrammer@gmail.com>
2024-02-05 03:10:39 +0600

    refactor

--- UM.Web/src/app/core/default-layout/_nav.ts
+++ UM.Web/src/app/core/default-layout/_nav.ts
@@ -1,12 +1,3 @@
 export const navItems: any[] = [
-  {
-    name: 'Dashboard',
-    url: '/dashboard',
-    iconComponent: { name: 'nav-icon fas fa-tachometer-alt' }
-  },
-  {
-    name: 'Users',
-    url: '/users',
-    iconComponent: { name: 'nav-icon fas fa-regular fa-user'}
-  }
+
 ];
